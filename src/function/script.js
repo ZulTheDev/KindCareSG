a96fad1dc8d35a4de121ae38cbd73938e7e5bd1c
Zul <76121296+ZulTheDev@users.noreply.github.com>
2023-11-16 22:05:21 +0800

    Update script.js

--- src/function/script.js
+++ src/function/script.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'Learn')));
 
 // Server-Side-Rendering
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Change this line
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Dynamic route for learn directory
@@ -29,4 +29,40 @@ app.listen(port, () => {
     console.log(`Server successfully loaded in http://localhost:${port}`);
 });
 
-// ... (rest of your code remains the same)
+//Collapsable text and content
+function toggleCollapse(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.classList.toggle('hidden');
+}
+//Sub menu for  learn directory
+function toggleSubmenu(submenuType) {
+    var submenu = null;
+
+    //Statement which one user click on
+    if (submenuType === 'Learn') {
+        submenu = document.getElementById('submenu');
+    } else if (submenuType === 'Hotline') {
+        submenu = document.getElementById('subhotline');
+    } else if (submenuType === 'Therapy') {
+        submenu = document.getElementById('subtherapy');
+    }
+
+    // Toggle hidden
+    if (submenu) {
+        submenu.classList.toggle('hidden');
+    }
+}
+
+function openCat(catId) {
+    if (catId === 'NeedTalk') {
+        // Redirect to another HTML page
+        window.location.href = '/services/';
+    }
+    else if (catId === 'LearnMentalHealth') {
+        window.location.href = '../Learn/main.html';
+    }
+}
+
+function Home(HomeBut) {
+    window.location.href = '../index.html';
+}
